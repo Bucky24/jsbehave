f0648cf6d53011c78a0942c51543d5c11e7227c7
Bucky24 <rwijtman@gmail.com>
2020-07-29 18:33:14 -0700

    some additional lines

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # jsbehave
 A module that allows an easy way to write browser based testing in Selenium
+
+## Operations
+
+`open <browser>`
+`navigate to <url>`
+`type <text> into <selector>`
+`wait for title to be <text>`
+`[test <string>]`
+`[endtest]`
+`click <selector>`
+`sleep <number>`
+`wait until located <selector>`
+
+## Text
+
+Text that is wrapped in double quotes is treated as text
+
+Text that is return sends the enter key to the element
+
+## Selector
+
+Selectors are of the format `type=value`
+
+| Type | Description |
+| ---- | ----------- |
+| name | Searches by element name |
+| text | Searches for elements containing text |
+| selector | Searches for elements matching css selector |
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 const fs = require("fs");
 const { EOL } = require('os');
 
-console.log(process.argv);
-
 const fileName = process.argv[2];
 
 const contents = fs.readFileSync(fileName, "utf-8");
@@ -19,6 +17,20 @@ function driver() {
     return getVariable("jsbehave.driver");
 }
 
+function getSelector(selector) {
+    const [type, ...other] = selector.split("=");
+
+    const finalVal = other.join("=");
+
+    if (type === "name") {
+        return By.name(finalVal);
+    } else if (type === "text") {
+        return By.xpath(`//*[text()="${finalVal}"]`);
+    } else if (type === "selector") {
+        return By.css(finalVal);
+    }
+}
+
 async function openBrowser([ browser ]) {
     const driver = await new Builder().forBrowser(browser).build();
     variables["jsbehave.driver"] = driver;
@@ -28,44 +40,105 @@ function goToPage([ url ]) {
     return driver().get(url);
 }
 
+const keyLookup = {
+    "return": Key.RETURN,
+};
+
+function typeKeys([ string, selector ]) {
+    const sel = getSelector(selector);
+
+    const element = driver().findElement(sel);
+
+    if (string.startsWith("\"") && string.endsWith("\"")) {
+        string = string.substr(1, string.length-2);
+    } else {
+        string = keyLookup[string];
+    }
+
+    return element.sendKeys(string);
+}
+
+function waitForTitle([ title ]) {
+    return driver().wait(until.titleIs(title), 5000)
+}
+
+function startTest([ test ]) {
+    variables["jbehave.activeTest"] = test;
+}
+
+function endTest() {
+    const test = variables["jbehave.activeTest"];
+
+    console.log(`Test ${test} - SUCCESS`);
+    delete variables["jbehave.activeTest"];
+}
+
+function clickElement([ selector ]) {
+    const sel = getSelector(selector);
+    const elem = driver().findElement(sel);
+    return elem.click();
+}
+
+function sleep([ seconds ]) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, seconds * 1000);
+    });
+}
+
+function waitForElement([ selector ]) {
+    const sel = getSelector(selector);
+    return driver().wait(until.elementLocated(sel, 5000));
+}
+
 const operations = {
     "open (.+)": openBrowser,
     "navigate to (.+)": goToPage,
+    "type (.+) into (.+)": typeKeys,
+    "wait for title to be \"(.+)\"": waitForTitle,
+    "\\[test (.+)\\]": startTest,
+    "\\[endtest\\]": endTest,
+    "click (.+)": clickElement,
+    "sleep (.+)": sleep,
+    "wait until located (.+)": waitForElement,
 };
 
 (async function example() {
-    for (let line of lines) {
-        let handled = false;
-        for (const operation in operations) {
-            const reg = RegExp(`^${operation}$`)
-            const matches = line.match(reg);
-            if (matches) {
-                const params = [...matches];
-                params.shift();
-                const func = operations[operation];
-                await func(params);
-                handled = true;
-                break;
+    const handleLines = async () => {
+        for (let line of lines) {
+            if (line === "") {
+                continue;
+            }
+            let handled = false;
+            for (const operation in operations) {
+                const reg = RegExp(`^${operation}$`)
+                const matches = line.match(reg);
+                if (matches) {
+                    const params = [...matches];
+                    params.shift();
+                    const func = operations[operation];
+                    try {
+                        await func(params);
+                    } catch (error) {
+                        const test = variables["jbehave.activeTest"];
+                        if (test) {
+                            console.log(`Test ${test} - FAILURE`, error);
+                        }
+                        return;
+                    }
+                    handled = true;
+                    break;
+                }
+            }
+            
+            if (!handled) {
+                console.log(`Unable to handle "${line}"`);
             }
         }
-        
-        if (!handled) {
-            console.log(`Unable to handle "${line}"`);
-        }
     }
+
+    await handleLines();
     
     if (variables["jsbehave.driver"]) {
-        await variables["jsbehave.driver"].quit();
+        //await variables["jsbehave.driver"].quit();
     }
 })();
-
-/*
-let driver = await new Builder().forBrowser('chrome').build();
-try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-} finally {
-    await driver.quit();
-}
-*/
